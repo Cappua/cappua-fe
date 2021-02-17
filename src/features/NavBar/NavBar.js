9ffcc52fec3fb3f4f9882f43f533d18cb5290cca
demaceo <hdemaceo@gmail.com>
2021-02-17 10:16:06 -0700

    Add hamburger nav menu functionality

--- src/features/NavBar/NavBar.js
+++ src/features/NavBar/NavBar.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+
 import "./NavBar.css";
 
 function NavBar() {
+  let navRef = useRef();
+  const [open, setOpen] = useState(false);
+  const toggleSettings = () => {
+    if (open) {
+      navRef.current.style.width = "0";
+      setOpen(false);
+    } else {
+      navRef.current.style.width = "20vw";
+      setOpen(true);
+    }
+  };
   return (
     <div className="navbar-container">
       <Link to="/">
         <img
           className="logo"
           src="https://media.giphy.com/media/FxfVpjpQb1kwKadK9L/giphy.gif"
-          //   src={"https://picsum.photos/1000?" + Math.random()}
           alt="logo"
         />
       </Link>
@@ -17,6 +29,21 @@ function NavBar() {
         <Link to="/halloffame">Hall of Fame</Link>
         <Link to="/competitions">Competitions</Link>
         <Link to="/login">Login</Link>
+      </div>
+
+      <div className="menu-container">
+        <i class="fas fa-bars" id="nav-icon" onClick={toggleSettings} />
+        <div className="sidenav" ref={navRef}>
+          <div id="sidenav-container">
+            <div id="sidenav-contents">
+              <Link to="/halloffame">Hall of Fame</Link>
+              <Link to="/competitions">Competitions</Link>
+              <Link to="/login" id="login">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
