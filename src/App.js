c6bc0e94193122c965db81a2ad98070446e33af4
demaceo <hdemaceo@gmail.com>
2021-02-17 01:41:40 -0700

    Display components in App

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./features/NavBar/NavBar.js";
+import Banner from "./features/Banner/Banner.js";
+import Body from "./features/Body/Body.js";
+import SoundBar from "./features/SoundBar/SoundBar.js";
+import  Trio  from "./features/Trio/Trio.js";
+import Login from "./features/Login/Login.js";
+import Announcement from './features/Announcement/Announcement.js'
+// import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <>
+              <Banner />
+              <Body />
+              <SoundBar />
+              <Trio />
+              <Announcement />
+            </>
+          )}
+        />
+        <Route exact path="/login" render={() => <Login />} />
+      </div>
+    </Router>
   );
 }
 
