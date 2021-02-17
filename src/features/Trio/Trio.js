4e5ab631a4b58826f818e9eafc8b92b6a29ff689
demaceo <hdemaceo@gmail.com>
2021-02-17 01:09:48 -0700

    Create Trio component

---
+++ src/features/Trio/Trio.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Trio.css";
+import trioData from "../../Utilities/trioData";
+import { nanoid } from "nanoid";
+// import { Link } from "react-router-dom";
+
+function Trio() {
+  const trioTabs = trioData.map((tab) => {
+    const { id, title, image, description, link } = tab;
+    return (
+        <section className="tab-container" key={nanoid()} id={id}>
+          <a href={link}>
+          <img
+            className="tab-img"
+            src={image}
+            key={id}
+            id={id}
+            alt="project-img"
+          />
+          </a>
+          <div className="tab-details" key={id}>
+            <h1 id="tab-title">{title}</h1>
+            <p id="tab-description">{description}</p>
+          </div>
+        </section>
+    );
+  });
+  return <section className="trio-container">{trioTabs}</section>;
+}
+
+export default Trio;
