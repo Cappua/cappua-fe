255909ddecf856277961b3cf51d709f03adf4c80
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 20:09:43 -0700

    Add upload button styling with icon

--- src/features/UploadTrack/UploadTrack.js
+++ src/features/UploadTrack/UploadTrack.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./UploadTrack.css";
-
+import uploadIcon from "../../assets/uploadIcon.png";
 const UploadTrack = () => {
   return (
     <div className="upload-track-container">
-      <input className="file-upload" type="file" />
+      {/* <input className="file-upload" type="file" accept="audio/*" /> */}
+      <label for="file-upload" className="file-upload">
+        <img src={uploadIcon} />
+      </label>
+      <input
+        id="file-upload"
+        style={{ visibility: "hidden", height: 0, width: 0 }}
+        type="file"
+      />
+      <label for="file-upload" className="file-upload-text">
+        Upload Your Track
+      </label>
     </div>
   );
 };
