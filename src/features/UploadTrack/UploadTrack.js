9d413d018c0a15f356a4e8c96c3f49b3874dab22
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 23:24:18 -0700

    update upload track button styling and layout

--- src/features/UploadTrack/UploadTrack.js
+++ src/features/UploadTrack/UploadTrack.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./UploadTrack.css";
 import uploadIcon from "../../assets/uploadIcon.png";
+
 const UploadTrack = () => {
   return (
     <div className="upload-track-container">
       {/* <input className="file-upload" type="file" accept="audio/*" /> */}
       <label for="file-upload" className="file-upload">
-        <img src={uploadIcon} />
+        <p className="file-upload-button-text">Upload Your Track</p>
+        <img src={uploadIcon} className="file-upload-icon" />
       </label>
       <input
         id="file-upload"
         style={{ visibility: "hidden", height: 0, width: 0 }}
         type="file"
       />
-      <label for="file-upload" className="file-upload-text">
-        Upload Your Track
-      </label>
     </div>
   );
 };
