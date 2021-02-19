a08746375be88b0b7f4cd3159da25b8ac147864a
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 21:37:07 -0700

    Instrumentals artwork component created with sample img

--- src/features/TrackPage/TrackPage.js
+++ src/features/TrackPage/TrackPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TrackCard from "../TrackCard/TrackCard.js";
-import TrackTitle from "../Tracktitle/TrackTitle.js";
+import InstrumentalsArtwork from "../InstrumentalsArtwork/InstrumentalArtwork.js";
 import TrackNav from "../TrackNav/TrackNav.js";
 import SoundBar from "../SoundBar/SoundBar.js";
 import UploadTrack from "../UploadTrack/UploadTrack.js";
@@ -10,7 +10,7 @@ const TrackPage = () => {
   return (
     <div className="track-page-container">
       <section className="title-uploader-container">
-        <TrackTitle />
+        <InstrumentalsArtwork />
         <UploadTrack />
       </section>
       <TrackCard />
