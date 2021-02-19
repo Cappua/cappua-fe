7c0cad3d30e7e6e1b0068db8cadfe488b5fd23b5
demaceo <hdemaceo@gmail.com>
2021-02-19 11:31:47 -0700

    Improve responsive design

--- src/features/NavBar/NavBar.js
+++ src/features/NavBar/NavBar.js
@@ -13,7 +13,7 @@ function NavBar() {
       navRef.current.style.width = "0";
       setOpen(false);
     } else {
-      navRef.current.style.width = "20vw";
+      navRef.current.style.width = "30vw";
       setOpen(true);
     }
   };
@@ -50,14 +50,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const LoginButton = () => {
-//   const { loginWithRedirect } = useAuth0();
-
-//   return <button onClick={() => loginWithRedirect()}>Log In</button>;
-// };
-
-// export default LoginButton;
