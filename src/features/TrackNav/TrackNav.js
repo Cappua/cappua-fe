d7a61ea0c361eeb5f29f31da91911533af6c88cd
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 20:57:44 -0700

    Changed title in track nav updated and formatted

--- src/features/TrackNav/TrackNav.js
+++ src/features/TrackNav/TrackNav.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './TrackNav.css';
+import React from "react";
+import "./TrackNav.css";
 
 const TrackNav = () => {
   return (
     <div className="tracknav-container">
-      <h3 className="contributors-title">Contributors</h3>
+      <h3 className="the-ranks-title">The Ranks</h3>
       <div className="track-sort-container">
-        <input type="search" className="search-contributors"/>
+        <input type="search" className="search-contributors" />
         <select className="track-sort-dropdown">
           <option value="newest">Newest</option>
           <option value="oldest">Oldest</option>
@@ -15,7 +15,7 @@ const TrackNav = () => {
         </select>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TrackNav;
