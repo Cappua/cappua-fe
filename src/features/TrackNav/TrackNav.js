ac8d88edcd23f1dea6a37ab428d1f15a360cfc42
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 21:12:05 -0700

    upadate the track nav

--- src/features/TrackNav/TrackNav.js
+++ src/features/TrackNav/TrackNav.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./TrackNav.css";
+import searchIcon from "../../assets/searchIcon.png";
 
 const TrackNav = () => {
   return (
     <div className="tracknav-container">
       <h3 className="the-ranks-title">The Ranks</h3>
       <div className="track-sort-container">
-        <input type="search" className="search-contributors" />
+        <form className="search-the-ranks-form">
+          <input
+            type="text"
+            className="search-the-ranks"
+            placeholder="Search the ranks"
+          />
+          <button className="search-the-ranks-button">
+            <img src={searchIcon} height="25px" width="25px" />
+          </button>
+        </form>
         <select className="track-sort-dropdown">
           <option value="newest">Newest</option>
           <option value="oldest">Oldest</option>
