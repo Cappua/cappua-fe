4934e3404e31eef27f8fbe65180ec562375d4e84
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-18 19:09:02 -0700

    Adjust track page layout

--- src/features/TrackPage/TrackPage.js
+++ src/features/TrackPage/TrackPage.js
@@ -4,12 +4,15 @@ import TrackTitle from "../Tracktitle/TrackTitle.js";
 import TrackNav from "../TrackNav/TrackNav.js";
 import SoundBar from "../SoundBar/SoundBar.js";
 import UploadTrack from "../UploadTrack/UploadTrack.js";
+import "./TrackPage.css";
 
 const TrackPage = () => {
   return (
     <div className="track-page-container">
-      <TrackTitle />
-      <UploadTrack />
+      <section className="title-uploader-container">
+        <TrackTitle />
+        <UploadTrack />
+      </section>
       <TrackCard />
       <SoundBar />
       <TrackNav />
