09b27829efcb592c96771366ac073432df013bf0
demaceo <hdemaceo@gmail.com>
2021-02-20 21:11:55 -0700

    Create Song component

---
+++ src/features/AudioPlayer/Song.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./audioPlayer.css";
+
+
+function Song(props) {
+  const { songName, songArtist } = props;
+  return (
+    <div className="song">
+      <h1 className="song-title">{songName}</h1>
+      <h2 className="song-artist">{songArtist}</h2>
+    </div>
+  );
+}
+
+export default Song;
