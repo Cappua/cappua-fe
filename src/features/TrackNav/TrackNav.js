9131f020bf3180e6c2bdbb31ad2e71dc655c6cb7
demaceo <hdemaceo@gmail.com>
2021-02-20 21:16:59 -0700

    Adjusts stylings and positioning

--- src/features/TrackNav/TrackNav.js
+++ src/features/TrackNav/TrackNav.js
@@ -4,27 +4,32 @@ import searchIcon from "../../assets/searchIcon.png";
 
 const TrackNav = () => {
   return (
-    <div className="tracknav-container">
-      <h3 className="the-ranks-title">The Ranks</h3>
+    <section className="tracknav-container">      
       <div className="track-sort-container">
-        <form className="search-the-ranks-form">
-          <input
-            type="text"
-            className="search-the-ranks"
-            placeholder="Search the ranks"
-          />
-          <button className="search-the-ranks-button">
-            <img src={searchIcon} height="25px" width="25px" />
-          </button>
-        </form>
-        <select className="track-sort-dropdown">
-          <option value="newest">Newest</option>
-          <option value="oldest">Oldest</option>
-          <option value="a-z">A-Z</option>
-          <option value="z-a">Z-A</option>
-        </select>
+        <h3 className="the-ranks-title">The Ranks</h3>
+
+        <div class="search-sort-container">
+          <form className="search-ranks-container">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search The Ranks"
+            />
+            <a class="search-btn">
+              <i class="fas fa-search" />
+            </a>
+          </form>
+          <select className="track-sort-dropdown">
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="a-z">A-Z</option>
+            <option value="z-a">Z-A</option>
+          </select>
+        </div>
+
+
       </div>
-    </div>
+    </section>
   );
 };
 
