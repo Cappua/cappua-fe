cfee61445301ee5840f82055f89c4b08110debd6
demaceo <hdemaceo@gmail.com>
2021-02-20 21:10:47 -0700

    Create mock user data objects

---
+++ src/assets/_sampleData/sampeUserData.js
@@ -0,0 +1,37 @@
+import samplePic1 from "./sampleUser1.jpg";
+import samplePic2 from "./sampleUser2.jpg";
+import samplePic3 from "./sampleUser3.jpg";
+import samplePic4 from "./mad_villan.png";
+
+const userData = [
+  {
+    id: 1,
+    artist: "Mad_Villain",
+    image: samplePic4,
+    song: "Whew!",
+    link: "/featured-artist",
+  },
+  {
+    id: 2,
+    artist: "Lil Killah",
+    image: samplePic3,
+    song: "Make Money Fast",
+    link: "/featured-artist",
+  },
+  {
+    id: 3,
+    artist: "KAFC",
+    image: samplePic2,
+    song: "fingy cuffs",
+    link: "/featured-artist",
+  },
+  {
+    id: 4,
+    artist: "Steve",
+    image: samplePic1,
+    song: "Forshizzle",
+    link: "/featured-artist",
+  },
+];
+
+export default userData;
