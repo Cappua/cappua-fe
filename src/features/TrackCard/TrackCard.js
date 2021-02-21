aabff0da0282ce64c94b2b851f182504a3852e8c
demaceo <hdemaceo@gmail.com>
2021-02-21 01:01:35 -0700

    Import dylon.mp3 to TrackCards

--- src/features/TrackCard/TrackCard.js
+++ src/features/TrackCard/TrackCard.js
@@ -1,22 +1,43 @@
 import React from "react";
 import "./TrackCard.css";
 import userData from "../../assets/_sampleData/sampeUserData";
+// import AudioPlayer from "../AudioPlayer/AudioPlayer";
+import dylon from "../../assets/dylon.mp3";
 
 const TrackCard = () => {
-
   const trackCards = userData.map((card) => {
     const { id, artist, image, song, link, color } = card;
 
     return (
-      <div key={id} className="trackcard-container">
-        <p className="user-order">
-          <i className="fa fa-award" style={{color: `${color}`}}></i>
+      <section
+        className="trackcard-container"
+        style={{
+          background: `url(${image}) no-repeat center center fixed`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div
+          key={id}
+          className="trackcard"
+          style={{
+            backdropFilter: "blur(30px)",
+          }}
+        >
+          <p className="user-order">
+            <i className={color ? `fa fa-award` : ''} style={{ color: `${color}` }}></i> 
+            
 
-          <img src={image} className="profile-img" />
-        </p>
-
-        <p className="track-username">{artist}</p>
-      </div>
+            <img src={image} className="profile-img" />
+          </p>
+          <div className="audio-container">
+            <audio className="audiotrack" controls>
+              <source src={dylon} />
+              Your browser does not support the <code>audio</code> element.
+            </audio>
+          </div>
+          <p className="track-username">{song} - {artist}</p>
+        </div>
+      </section>
     );
   });
   return <section className="all-cards-container">{trackCards}</section>;
