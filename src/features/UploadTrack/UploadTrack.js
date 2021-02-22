0c369e27a82b1e2b7edcca42a49f62e4f4d82f76
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-21 21:54:13 -0700

    Upload track working and posting to backend

--- src/features/UploadTrack/UploadTrack.js
+++ src/features/UploadTrack/UploadTrack.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UploadTrack.css";
-import uploadIcon from "../../assets/uploadIcon.png";
+import axios from "axios";
 
 const UploadTrack = () => {
+  const [file, setFile] = useState(null);
+
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("audio", file);
+    formData.append("month", 1);
+    formData.append("year", 2021);
+    formData.append("type", "track");
+
+    axios({
+      method: "post",
+      url: "http://localhost:8000/api/v1/upload",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="upload-track-container">
-      {/* <input className="file-upload" type="file" accept="audio/*" /> */}
-      <label htmlFor="file-upload" className="file-upload">
-        <p className="file-upload-button-text">Upload Your Track</p>
-        <img src={uploadIcon} className="file-upload-icon" />
-      </label>
-      <input
-        id="file-upload"
-        style={{ visibility: "hidden", height: 0, width: 0 }}
-        type="file"
-      />
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}>
+        <input
+          id="file-upload"
+          type="file"
+          onChange={(event) => {
+            handleChange(event);
+          }}
+          accept=".mp3,audio/*"
+        />
+        <button className={file ? "upload-button" : "disabled"}>
+          Submit File
+        </button>
+      </form>
     </div>
   );
 };
