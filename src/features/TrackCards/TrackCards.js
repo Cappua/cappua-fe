71b3aeb612d324d3abd0864562f826f06a6e8043
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-23 23:18:24 -0700

    Query all verses for competition page and display as track cards

---
+++ src/features/TrackCards/TrackCards.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./TrackCards.css";
+import userData from "../../assets/_sampleData/sampeUserData";
+import dylon from "../../assets/dylon.mp3";
+import { useQuery, gql } from "@apollo/client";
+import { GET_ALL_VERSES } from "../../GraphQL/queries.js";
+
+const TrackCards = () => {
+  const [tracks, setTracks] = useState([]);
+  const { error, loading, data } = useQuery(GET_ALL_VERSES);
+
+  useEffect(() => {
+    if (data) {
+      const sortedTracks = () =>
+        data.verses.sort(function (a, b) {
+          return b.votes - a.votes;
+        });
+      setTracks(sortedTracks);
+    }
+  }, [data]);
+
+  let trackCards = tracks.map((card, i) => {
+    const { id, artist, image, audioPath, link, votes } = card;
+    return (
+      <section key={i} className="trackcards-container">
+        <div className="trackcards">
+          <p className="user-order">
+            <i className={i <= 2 ? `fa fa-award top-${i}` : ""}></i>
+            <img src={image} className="profile-img" />
+          </p>
+          <div className="audio-container">
+            <div className="track-username">
+              <h1 className="track-title">{audioPath}</h1>
+              <h1 className="track-artist">{artist}</h1>
+            </div>
+            <audio className="audiotrack" controls>
+              <source
+                src={`https://testabucketblazeit.s3-us-west-1.amazonaws.com/${audioPath}`}
+              />
+              Your browser does not support the <code>audio</code> element.
+            </audio>
+          </div>
+          <div className="play-count">
+            <h1 className="votes" id={id}>
+              {votes}
+            </h1>
+            <i className="fas fa-long-arrow-alt-up vote-icon" id={id}></i>
+          </div>
+        </div>
+      </section>
+    );
+  });
+  return <section className="all-cards-container">{trackCards}</section>;
+};
+
+export default TrackCards;
