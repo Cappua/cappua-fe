69e7d14cc1a4bf3f2e28a3a5614f866a1f5f6651
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-25 21:08:51 -0700

    Rotuer moved into file -- ApolloProvider added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { ApolloProvider } from "@apollo/client";
+import { errorLink, client } from "./ApolloClient/apolloClient.js";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-e23-u4dy.us.auth0.com"
-    clientId="9yOyW6Kmq0X9uU3a6IvfYOTPzzS6StBX"
-    redirectUri="http://localhost:8000/">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>,
+  <ApolloProvider client={client}>
+    <Auth0Provider
+      domain="dev-e23-u4dy.us.auth0.com"
+      clientId="9yOyW6Kmq0X9uU3a6IvfYOTPzzS6StBX"
+      redirectUri="http://localhost:8000/">
+      <React.StrictMode>
+        <Router>
+          <App />
+        </Router>
+      </React.StrictMode>
+    </Auth0Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
