15326cbe8046fd0310321d18a234bba087f44ae0
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-25 21:07:20 -0700

    Apply state with query -- competition context provider added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
 import NavBar from "./features/NavBar/NavBar.js";
 import Banner from "./features/Banner/Banner.js";
 import Body from "./features/Body/Body.js";
@@ -7,37 +7,43 @@ import Trio from "./features/Trio/Trio.js";
 import Announcement from "./features/Announcement/Announcement.js";
 import TrackPage from "./features/TrackPage/TrackPage.js";
 import AudioPlayer from "./features//AudioPlayer/AudioPlayer.js";
-import Footer from "./features/Footer/Footer";
 import Winners from "./features/Winners/Winners";
 import "./App.css";
-import { ApolloProvider } from "@apollo/client";
-import { errorLink, client } from "./ApolloClient/apolloClient.js";
+import { useQuery } from "@apollo/client";
+import { CompetitionContext } from "./CompetitionContext";
+import { GET_VERSES_BY_COMPETITION } from "./GraphQL/queries";
 
 function App() {
+  const [competition, setCompetition] = useState([]);
+  const { error, loading, data } = useQuery(GET_VERSES_BY_COMPETITION);
+
+  useEffect(() => {
+    setCompetition(data);
+  }, [data]);
+
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <>
-                <Banner />
-                <Body />
-                <AudioPlayer />
-                <Trio />
-                <Announcement />
-              </>
-            )}
-          />
-          <Route exact path="/competitions" render={() => <TrackPage />} />
-          <Route exact path="/winners" render={() => <Winners />} />
-        </div>
-        {/* <Footer /> */}
-      </Router>
-    </ApolloProvider>
+    <CompetitionContext.Provider value={competition}>
+      <div className="App">
+        <NavBar />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <>
+              <Banner />
+              <Body />
+              <AudioPlayer />
+              <Trio />
+              <Announcement />
+            </>
+          )}
+        />
+        <Route exact path="/competitions" render={() => <TrackPage />} />
+        <Route exact path="/winners" render={() => <Winners />} />
+      </div>
+
+      {/* <Footer /> */}
+    </CompetitionContext.Provider>
   );
 }
 
