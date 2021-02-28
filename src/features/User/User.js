e388e069f0e529c5671e967984f549ded9c22627
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-28 13:55:18 -0700

    Setting global user from user component using context

--- src/features/User/User.js
+++ src/features/User/User.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import UserContext from "../../UserContext";
 import "./User.css";
 
 const Profile = () => {
@@ -11,27 +12,35 @@ const Profile = () => {
     logout,
   } = useAuth0();
 
+  const { setUserInfo } = useContext(UserContext);
+
+  if (user) {
+    setUserInfo(user);
+  }
 
   if (!isAuthenticated) {
     return (
       <button className="auth0" onClick={() => loginWithRedirect()}>
-
         Log In
       </button>
     );
   }
+
+  // useEffect(() => {
+  //   setUserInfo(user);
+  // }, []);
+
   return (
     isAuthenticated && (
       <div className="user-info-container">
         <div className="user-info">
           <img id="user-icon" src={user.picture} alt={user.name}></img>
-          <i class="fa fa-caret-down"></i>
+          <i className="fa fa-caret-down"></i>
           <div className="user-dropdown">
             <div className="user-dropdown--item">
               <button
                 className="auth0 logout"
-                onClick={() => logout({ returnTo: window.location.origin })}
-              >
+                onClick={() => logout({ returnTo: window.location.origin })}>
                 Log Out
               </button>
             </div>
