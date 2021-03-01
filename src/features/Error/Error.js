20e53cab3b6b6bff771e233d1536d56932963d31
demaceo <hdemaceo@gmail.com>
2021-03-01 15:57:39 -0700

    Style Error page

--- src/features/Error/Error.js
+++ src/features/Error/Error.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Error.css";
+import AlbumCard from '../AlbumCard/AlbumCard';
+import cappuaLogo from "../../assets/cappualogo.png";
 
 const Error = () => {
-  return <div className="error-page">ERROR...</div>;
+  return (
+    <div
+      className="error-page"
+      // style={{ backgroundImage: `url(${cappuaLogo})` }}
+    >
+      <div className="error">ERROR...😬</div>
+    </div>
+  );
 };
 
 export default Error;
