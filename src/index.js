9d08acea111c17b6332d2c53540085b01d4b1cc5
demaceo <hdemaceo@gmail.com>
2021-03-01 15:58:46 -0700

    Minor changes (styling / margin adjustments)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
