39ad5caa6cbaa739c01622e124bad45ee1c369af
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-02 11:02:44 -0700

    Clean up file

--- src/features/Competition/Competition.js
+++ src/features/Competition/Competition.js
@@ -1,14 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import TrackCards from "../TrackCards/TrackCards.js";
 import UploadTrack from "../UploadTrack/UploadTrack.js";
 import AudioPlayer from "../AudioPlayer/AudioPlayer.js";
-import UserContext from "../../UserContext";
-import { CompetitionContext } from "../../CompetitionContext";
 import Theme from "../Theme/Theme.js";
 import "./Competition.css";
 
 const Competition = () => {
-
   return (
     <section className="track-page-container">
       <div className="theme-container">
