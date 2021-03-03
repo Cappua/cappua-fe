81f4d6c8ee1922b8f3add33bb46ccd509755fd75
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-02 18:06:01 -0700

    Removed Announcement

--- src/App.js
+++ src/App.js
@@ -53,7 +53,6 @@ const App = () => {
                     <Body />
                     <AudioPlayer />
                     <Trio />
-                    <Announcement />
                   </>
                 )}
               />
