2bcb9a75bfbf6f8671a39b388b4da5497436c058
demaceo <hdemaceo@gmail.com>
2021-03-02 17:53:57 -0700

    Display audiotracks to Olympians

--- src/features/Olympus/Olympus.js
+++ src/features/Olympus/Olympus.js
@@ -4,35 +4,72 @@ import { GET_ALL_VERSES } from "../../GraphQL/queries.js";
 import React, { useEffect, useState } from "react";
 
 const Olympus = () => {
-  const [olympus, setOlympus] = useState([]);
+  // const [olympus, setOlympus] = useState([]);
   const { error, loading, data } = useQuery(GET_ALL_VERSES);
   const [january, setJanuary] = useState([]);
   const [february, setFebruary] = useState([]);
 
+  // const [tracks, setTracks] = useState(null);
+  // const [trio, setTrio] = useState([]);
+  // const { error, loading, data } = useQuery(GET_ALL_VERSES);
+
+  //   useEffect(() => {
+  //     if (data) {
+  //       setTracks(data.verses);
+  //     }
+  //   }, [data]);
+
+  //   useEffect(() => {
+  //     if (tracks) {
+  //       let sorted = tracks.slice().sort((a, b) => {
+  //         return b.voteCount - a.voteCount;
+  //       });
+  //       setTrio([sorted[0], sorted[1], sorted[2]]);
+  //     }
+  //   }, [tracks]);
+
   useEffect(() => {
     if (data) {
-      setOlympus(data.verses);
+      // setOlympus(data.verses);
       setJanuary([data.verses[0]]);
       setFebruary([data.verses[5]]);
     }
   }, [data]);
 
   const janTracks = january.map((card, i) => {
-    const { user, artist, audioPath, title, voteCount } = card;
+    const { user, audioPath, title} = card;
     return (
-      <div className="olympus-track january" key={i} id={i}>
+      <div className="olympus-track january" key={i} id={i + 1}>
         <img className="olympian-img" src={user.image}></img>
-        <div>{user.name}</div>
+        <div className="olympian-audiotrack">
+          <div className="olympian-username">
+            <h1 className="olympian-song">{title}</h1>
+            <h1 className="olympian-artist">{user.name}</h1>
+          </div>
+          <audio className="audiotrack" controls>
+            <source src={`http://d1nb1e3bp5hs25.cloudfront.net${audioPath}`} />
+            Your browser does not support the <code>audio</code> element.
+          </audio>
+        </div>
       </div>
     );
   });
 
   const febTracks = february.map((card, i) => {
-    const { user, artist, audioPath, title, voteCount } = card;
+    const { user, audioPath, title } = card;
     return (
       <div className="olympus-track february" key={i} id={i + 1}>
         <img className="olympian-img" src={user.image}></img>
-        <div>{user.name}</div>
+        <div className="olympian-audiotrack">
+          <div className="olympian-username">
+            <h1 className="olympian-song">{title}</h1>
+            <h2 className="olympian-artist">{user.name}</h2>
+          </div>
+          <audio className="audiotrack" controls>
+            <source src={`http://d1nb1e3bp5hs25.cloudfront.net${audioPath}`} />
+            Your browser does not support the <code>audio</code> element.
+          </audio>
+        </div>
       </div>
     );
   });
